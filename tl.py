ed113c9b1478e5abbae2e86e66d19e3330ccb99c
Marcin Kurczewski <mkurczew@gmail.com>
2013-07-01 11:10:53 +0200

    Fixed color library problems

--- tl.py
+++ tl.py
@@ -6,7 +6,11 @@ import json
 import http.client
 from urllib.parse import urlencode
 from urllib.parse import urlparse
-from lib.colors import colorize
+try:
+	import colorama
+	colorama.init()
+except ImportError:
+	pass
 
 def get_language(code):
 	synonyms = {"auto":["auto","detect"],"af":["af","afrikaans"],"sq":["sq","albanian"],"ar":["ar","arabic"],"hy":["hy","armenian"],"az":["az","azerbaijani"],"eu":["eu","basque"],"be":["be","belarusian"],"bn":["bn","bengali"],"bg":["bg","bulgarian"],"ca":["ca","catalan"],"zh-CN":["cn","zh-cn","china","chinese"],"hr":["hr","croatian"],"cs":["cs","cz","czech"],"da":["da","danish"],"nl":["nl","dutch"],"en":["en","eng","english"],"et":["et","est","estonian"],"tl":["tl","filipino"],"fi":["fi","finnish"],"fr":["fr","french"],"gl":["gl","galician"],"ka":["ka","georgian"],"de":["de","german"],"el":["el","greek"],"gu":["gu","gujarati"],"ht":["ht","haitian","creole"],"iw":["iw","hebrew"],"hi":["hi","hindi"],"hu":["hu","hungarian"],"is":["is","icelandic"],"id":["id","indonesian"],"ga":["ga","irish"],"it":["it","italian"],"ja":["ja","jp","jap","japanese"],"kn":["kn","kannada"],"ko":["ko","korean"],"la":["la","latin"],"lv":["lv","latvian"],"lt":["lt","lithuanian"],"mk":["mk","macedonian"],"ms":["ms","malay"],"mt":["mt","maltese"],"no":["no","norwegian"],"fa":["fa","persian"],"pl":["pl","pol","polish"],"pt":["pt","portuguese"],"ro":["ro","romanian"],"ru":["ru","russian"],"sr":["sr","serbian"],"sk":["sk","slovak"],"sl":["sl","slovenian"],"es":["es","spanish"],"sw":["sw","swahili"],"sv":["sv","swedish"],"ta":["ta","tamil"],"te":["te","telugu"],"th":["th","thai"],"tr":["tr","turkish"],"uk":["uk","ukrainian"],"ur":["ur","urdu"],"vi":["vi","vietnamese"],"cy":["cy","welsh"],"yi":["yi","yiddish"]}
@@ -48,8 +52,15 @@ for text in sys.argv[3:]:
 	data = json.loads(content)
 	for sentence in data['sentences']:
 		print('  ', end=' ')
-		print(colorize(sentence['trans'], ['Fore.RED', 'Style.BRIGHT']), end=' ')
-		print(colorize('(Transliteration: {0})'.format(sentence['translit']), ['Fore.WHITE', 'Style.DIM']) if 'translit' in sentence and sentence['translit'] else '')
+		if colorama:
+			print(colorama.Fore.RED + colorama.Style.BRIGHT, end='')
+		print(sentence['trans'], end=' ')
+		if 'translit' in sentence and sentence['translit']:
+			if colorama:
+				print(colorama.Fore.WHITE + colorama.Style.DIM, end='')
+			print('(Transliteration: {0})'.format(sentence['translit']), end='')
+	if colorama:
+		print(colorama.Fore.RESET + colorama.Style.RESET_ALL)
 
 	if 'dict' in data:
 		for dict in sorted(data['dict'], key = lambda x: len(x['entry'])):
@@ -57,6 +68,13 @@ for text in sys.argv[3:]:
 			print('Dictionary ({0}):'.format(dict['pos']) if 'pos' in dict and dict['pos'] else 'Dictionary:')
 			for entry in dict['entry']:
 				print('  ', end=' ')
-				print(colorize(entry['word'], ['Fore.RED']), end=' ')
-				print(colorize('(Reverse translation: {0})'.format(', '.join(entry['reverse_translation'])), ['Fore.WHITE', 'Style.DIM']) if 'reverse_translation' in entry and entry['reverse_translation'] else '')
+				if colorama:
+					print(colorama.Fore.RED, end='')
+				print(entry['word'], end=' ')
+				if 'reverse_translation' in entry and entry['reverse_translation']:
+					if colorama:
+						print(colorama.Fore.WHITE + colorama.Style.DIM, end='')
+					print('(Reverse translation: {0})'.format(', '.join(entry['reverse_translation'])))
+					if colorama:
+						print(colorama.Fore.RESET + colorama.Style.RESET_ALL, end='')
 	print()
